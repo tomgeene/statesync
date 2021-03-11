c02589ec3527e56f04770265cf96c13590c32fb9
Tom Geene <tom@virtrixx.nl>
2021-03-11 10:25:06 +0100

    initial mix project

---
+++ lib/statesync.ex
@@ -0,0 +1,18 @@
+defmodule Statesync do
+  @moduledoc """
+  Documentation for `Statesync`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Statesync.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/statesync_test.exs
@@ -0,0 +1,8 @@
+defmodule StatesyncTest do
+  use ExUnit.Case
+  doctest Statesync
+
+  test "greets the world" do
+    assert Statesync.hello() == :world
+  end
+end
